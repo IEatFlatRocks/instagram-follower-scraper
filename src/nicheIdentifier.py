b90e334802d85f81ca1c3678f7130e44cb9d9251
Carter <carter.limster@gmail.com>
2025-04-30 11:01:11 -0700

    Added Niche Identification

---
+++ src/nicheIdentifier.py
@@ -0,0 +1,46 @@
+import os
+from dotenv import load_dotenv
+from groq import Groq
+
+
+load_dotenv()
+client = Groq(
+    api_key = os.getenv('GROQ_API_KEY'),
+)
+
+
+prompt = """
+You are a niche identifier. You will be given a bio and a niche.
+Your task is to determine if the bio is related to the niche. 
+Type a paragraph explaining if the bio is related to the niche.
+Then, make your last word "yes" or "no".
+Here is the bio and the niche:
+"""
+
+def isNiche(bio, niche):
+    reponse = requestChat(prompt + bio + ", " + niche)
+    print(reponse)
+    return reponse.split()[-1].lower() == "yes"
+
+def requestChat(prompt):    
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ],
+        model="llama-3.3-70b-versatile",
+    )
+
+    return (chat_completion.choices[0].message.content)
+
+
+def main():
+    bio = "Neurodiverse online wellness/mindset/mindfulness coach✨work shop based tips and strategies to help overcome our own...  more"
+    niche = "Lifestyle for sigmas"
+    result = isNiche(bio, niche)
+    print(f"Is the bio related to the niche? {result}")
+
+if __name__ == "__main__":
+    main()
